397df1c288416b20228b4fee532e7c6c0f0a5328
ogy <ba550b0e@gmail.com>
2019-05-21 22:37:47 +0900

    インデント修正

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -9,16 +9,16 @@
 window.addEventListener('chromeOpen', evt => {
 	console.log('event captured!');
 
-		const url = evt.detail.url;
-		if (url.startsWith('file://')) {
-			evt.preventDefault();
-			// 拡張が再読み込みされた場合エラーになるので捕捉
-			// Catch the error for the extension is reloaded.
-			try {
-				chrome.runtime.sendMessage({
-					method: 'openLocalFile',
-					localFileUrl: url,
-				});
-			} catch (e) {}
-		}
+	const url = evt.detail.url;
+	if (url.startsWith('file://')) {
+		evt.preventDefault();
+		// 拡張が再読み込みされた場合エラーになるので捕捉
+		// Catch the error for the extension is reloaded.
+		try {
+			chrome.runtime.sendMessage({
+				method: 'openLocalFile',
+				localFileUrl: url,
+			});
+		} catch (e) {}
+	}
 });
